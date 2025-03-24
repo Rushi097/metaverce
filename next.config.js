1d2d0efa94b859fc5154c54fdb49358cc73ebf21
Rushi097 <2297rushikesh@gmail.com>
2025-03-25 01:06:11 +0530

    1

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: false,
+    // appDir option removed as it's no longer supported in Next.js 15.2.3
   },
+  output: 'export', // Enable static exports
 };
 
 module.exports = nextConfig;
